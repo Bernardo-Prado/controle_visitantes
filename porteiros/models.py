98d3b2ba598209bb0d42148cf955583331959ee5
Bernardo Prado <bernardoprado22@yahoo.com.br>
2023-01-31 16:56:43 -0300

    Criando o cadastro do porteiro

--- porteiros/models.py
+++ porteiros/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Porteiro(models.Model):
+    nome_completo = models.CharField(
+        verbose_name="Nome Completo",
+        max_length=194,
+    )
+
+    cpf = models.CharField(
+        verbose_name="CPF",
+        max_length=11,
+    )
+
+    telefone = models.CharField(
+        verbose_name="Telefone de contato",
+        max_length=11,
+    )
