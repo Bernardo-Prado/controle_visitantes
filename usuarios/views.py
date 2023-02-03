b7d6625d61752f9590216679756769a72fe932b4
Bernardo Prado <bernardoprado22@yahoo.com.br>
2023-02-02 22:45:51 -0300

    Exibindo variáveis no template

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, "index.html")
+
+    context = {
+        "nome_pagina": "Início da dashboard",
+    }
+
+    return render(request, "index.html", context)
