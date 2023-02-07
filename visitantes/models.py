a10a90bbab920cd13f29b0baf15c327d1976a609
Bernardo Prado <bernardoprado22@yahoo.com.br>
2023-02-07 19:06:39 -0300

    Cria o model

--- visitantes/models.py
+++ visitantes/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Visitantes(models.Model):
+
+    nome_completo = models.CharField(
+        verbose_name="Nome Completo",
+        max_length=194
+    )
+
+# nome completo, cpf, data de nascimento, número da casa, placa do veiculo, horario de entrada-saida-autorização, nome de quem autorizou, nome do responsável pelo registro
