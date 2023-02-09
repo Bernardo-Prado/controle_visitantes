d19cc5760dbbdd3197d6ad2ca2ded85ae1fe0c4e
Bernardo Prado <bernardoprado22@yahoo.com.br>
2023-02-09 16:19:22 -0300

    Especifica os campos a serem exibidos

--- visitantes/forms.py
+++ visitantes/forms.py
@@ -2,7 +2,14 @@ from django import forms
 from visitantes.models import Visitante
 
 # Mapeia os campos automaticamente da classe Model
+# Define quais campos vão ser exibidos 
 class VisitanteForm(forms.ModelForm):
     class Meta:
         model = Visitante
-        fields = "__all__"
+        fields = [
+            "nome_completo", 
+            "cpf", 
+            "data_de_nascimento", 
+            "numero_casa", 
+            "placa_veiculo"
+        ]
